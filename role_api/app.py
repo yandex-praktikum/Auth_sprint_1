0a4501bd8076e7f497f23a23c2ccdb10a920ca9d
Aleksey Komissarov <ad3002@gmail.com>
2022-06-27 11:04:02 +0300

    stash before merge

--- role_api/app.py
+++ role_api/app.py
@@ -2,28 +2,37 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-some_template = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
-<p>Вы искали: {}</p>
-<form method="get" action="/">
-<input name="query">
-</form>
-</body>
-</html>
+@app.route("/roles", ["GET"])
+def get_roles():
+    pass
 
-'''
+@app.route("/role/{role_id}", ["GET"])
+def get_role(role_id):
+    pass
 
+@app.route("/role/{role_id}", ["PUT"])
+def put_role(role_id):
+    pass
 
-@app.route("/")
-def main():
-    return some_template.format(request.args.get('query', default=''))
+@app.route("/role/{role_id}", ["DELETE"])
+def delete_role(role_id):
+    pass
 
+@app.route("/user/{user_id}", ["GET"])
+def get_user_roles(user_id):
+    pass
+
+@app.route("/user/{user_id}", ["PUT"])
+def add_role_to_user(user_id):
+    pass
+
+@app.route("/user/{user_id}", ["DELETE"])
+def delete_role_from_user(user_id):
+    pass
+
+@app.route("/check", ["POST"])
+def check_role(user_id):
+    pass
 
 if __name__ == '__main__':
     app.run(
--- role_api/pywsgi.py
+++ role_api/pywsgi.py
@@ -5,6 +5,5 @@ monkey.patch_all()
 from gevent.pywsgi import WSGIServer
 from app import app
 
-
-http_server = WSGIServer(('', 5000), app)
+http_server = WSGIServer(('', 5001), app)
 http_server.serve_forever() 
