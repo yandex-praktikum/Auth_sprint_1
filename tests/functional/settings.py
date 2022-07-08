5b2680c3667f68b2e5fa01ca1685c8166cd6ad6a
liubovant <antyufrieva@phystech.edu>
2022-07-08 16:40:49 +0300

    before checkout

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -22,10 +22,10 @@ class Settings(BaseSettings):
     redis_max_size: int = 10
     redis_cache_expire_min: int = 60 * 5
 
-    search_api_host: str = Field("127.0.0.1", env="SEARCH_APP_HOST")
+    search_api_host: str = Field("search_api", env="SEARCH_APP_HOST")
     search_api_port: int = Field(5003, env="SEARCH_APP_PORT")
 
-    es_host: str = Field("127.0.0.1", env="ELASTIC_HOST")
+    es_host: str = Field("postgres", env="ELASTIC_HOST")
     es_port: int = Field(5003, env="ELASTIC_PORT")
 
     postgres_user: str = Field("docker", env="POSTGRES_USER")
@@ -34,10 +34,10 @@ class Settings(BaseSettings):
     postgres_port: int = Field(10, env="POSTGRES_PORT")
     postgres_db: str = Field("content", env="POSTGRES_DB")
 
-    role_api_host: str = Field("127.0.0.1", env="ROLE_APP_HOST")
+    role_api_host: str = Field("role_api", env="ROLE_APP_HOST")
     role_api_port: int = Field(5002, env="ROLE_APP_PORT")
 
-    auth_api_host: str = Field("127.0.0.1", env="AUTH_APP_HOST")
+    auth_api_host: str = Field("auth_api", env="AUTH_APP_HOST")
     auth_api_port: int = Field(5001, env="AUTH_APP_PORT")
 
     JWT_SECRET_KEY: str = Field("JWT_SECRET_KEY", env="JWT_SECRET_KEY")
